54163cdc870f830b8dd7f210a000815aff8baf24
AtkinsJM <github@joshatkins.co.uk>
2019-12-24 14:33:04 +0000

    Updated gitignore.

---
+++ Source/WoodlandAdventure/AnimalPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AnimalPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WOODLANDADVENTURE_API AAnimalPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
