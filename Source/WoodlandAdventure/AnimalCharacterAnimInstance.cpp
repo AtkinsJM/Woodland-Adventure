0e17cf71a29b2ea1886b87f45c31dc1dddbdcb64
AtkinsJM <github@joshatkins.co.uk>
2019-12-25 15:35:28 +0000

    Set up basic movement animation blendspace for fox.

---
+++ Source/WoodlandAdventure/AnimalCharacterAnimInstance.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AnimalCharacterAnimInstance.h"
+#include "AnimalCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+void UAnimalCharacterAnimInstance::NativeInitializeAnimation()
+{
+	if (!Pawn)
+	{
+		Pawn = TryGetPawnOwner();
+	}
+	if (Pawn && !AnimalCharacter)
+	{
+		AnimalCharacter = Cast<AAnimalCharacter>(Pawn);
+	}
+}
+
+void UAnimalCharacterAnimInstance::UpdateAnimationProperties()
+{
+	if (Pawn)
+	{
+		if (LastYaw == 0.0f) { LastYaw = Pawn->GetActorRotation().Yaw; }
+		FVector Speed = Pawn->GetVelocity();
+		FVector LateralSpeed = FVector(Speed.X, Speed.Y, 0.0f);
+		ForwardSpeed = LateralSpeed.Size();
+		UE_LOG(LogTemp, Warning, TEXT("Speed: %f"), ForwardSpeed);
+		YawDelta = Pawn->GetActorRotation().Yaw - LastYaw;
+		if (YawDelta > 180.0f) { YawDelta -= 360.0f; }
+		if (YawDelta < -180.0f) { YawDelta += 360.0f; }
+		LastYaw = Pawn->GetActorRotation().Yaw;
+		
+		bIsInAir = Pawn->GetMovementComponent()->IsFalling();
+	}
+}
---
+++ Source/WoodlandAdventure/AnimalCharacterAnimInstance.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "AnimalCharacterAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WOODLANDADVENTURE_API UAnimalCharacterAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character")
+	class APawn* Pawn;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Character")
+	class AAnimalCharacter* AnimalCharacter;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement")
+	float ForwardSpeed;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement")
+	float YawDelta;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement")
+	bool bIsInAir;
+
+protected:
+	virtual void NativeInitializeAnimation() override;
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "Animation Properties")
+	void UpdateAnimationProperties();
+
+private:
+	float LastYaw;
+};
